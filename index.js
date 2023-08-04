d4c0324197973bb8102b8751ffdc7352a0e6f7dd
SheteUC <sheteaa@mail.uc.edu>
2023-08-04 17:58:39 -0400

    player and enemy initiation

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 // Description: Main file for the game
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
 // Set canvas size
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 1024
+canvas.height = 576
 
 // Set background color
-c.fillRect(0, 0, canvas.width, canvas.height);
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += 10
+    }
+}
+
+const player = new Sprite({position: {x: 0, y: 0}, velocity: {x: 0, y: 0}})
+
+
+const enemy = new Sprite({position:{x: 400, y: 100}, velocity: {x: 0, y: 0}})
+
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    player.update()
+    enemy.update()
+}
+
+animate()
